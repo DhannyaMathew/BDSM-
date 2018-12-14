95f73c441ac0d4185b964c35e7732094b1c82b99
DhannyaMathew <mathewdhannya@gmail.com>
2018-12-14 15:21:09 +0200

    NavMesh3D
    No 2D sprites input yet

---
+++ 2018GameJam2D/Assets/Scripts/Agent.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Agent : MonoBehaviour {
+
+	private NavMeshAgent agent;
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<NavMeshAgent> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//assigning target to agent component
+		if (Input.GetMouseButtonDown(0)){
+			//Create raycast to find new target
+			//change world cooridnates of mouse into world screen coordinates using camera
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			if (Physics.Raycast (ray,out hit)) {
+				if (hit.collider.tag == "Ground") { //if select ground, set new destination
+					agent.SetDestination(hit.point);
+				}
+			}
+		}
+	}
+}
