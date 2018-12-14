aae90783ab94a5b558e76736c76bd857f009a766
DhannyaMathew <mathewdhannya@gmail.com>
2018-12-14 22:42:10 +0200

    test changes

---
+++ 2018GameJam2D/Assets/Scripts/Navmeshtest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Navmeshtest : MonoBehaviour {
+	public GameObject test;
+	public Transform parent;
+	// Use this for initialization
+	void Start () {
+		Instantiate (test, new Vector3 (0, -2, -1), Quaternion.identity, parent);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
