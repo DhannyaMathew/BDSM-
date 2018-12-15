105d54a16dc440b62366c79baca462d0e72f7574
DhannyaMathew <mathewdhannya@gmail.com>
2018-12-15 17:31:55 +0200

    Added Some Time Scripts

---
+++ 2018GameJam2D/Assets/Scripts/GameManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+	private bool GamePaused;
+	private GameObject PauseScreen;
+	public static GameManager instance = null;
+
+	void Awake ()
+	{
+		if (instance == null) {
+			instance = this;
+		} else if (instance != null) {
+			Destroy (gameObject);
+		}
+	}
+
+	void Start () {
+		PauseScreen = GameObject.FindGameObjectWithTag ("PauseScreen");
+	}
+	
+	void Pause ()
+	{
+		
+		PauseScreen.SetActive (true);
+		//Time.timeScale = 0f;				//freezes the game
+		GamePaused = true;
+	}
+
+	public void Resume ()
+	{
+		PauseScreen.SetActive (false);
+		//Time.timeScale = 1f;				//resumes the game
+		GamePaused = false;
+	}
+
+	public void Restart ()
+	{
+		//Time.timeScale = 1f;
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex);		
+	}
+	public void Menu ()
+	{
+		//Time.timeScale = 1f;
+		SceneManager.LoadScene ("Intro");
+
+	}
+	public void NextLevel ()
+	{
+		//Time.timeScale = 1f;
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1);
+
+	}
+	public void Quit ()
+	{
+		Application.Quit ();
+	}
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			Quit ();
+		}
+		if (Input.GetKeyDown (KeyCode.P)) {
+			if (GamePaused == true) {
+				Resume ();
+			} else {
+				Pause ();
+			}
+		}
+	}
+}
---
+++ 2018GameJam2D/Assets/Scripts/WorkStation.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WorkStation : MonoBehaviour {
+	public float MaxTime;
+	private float CurrTime;
+	public Slider TimeSlider;
+	public Image FillImage;
+	public Color FullTimeColour = Color.green;
+	public Color LowTimeColour = Color.red;
+	public AudioClip TimeDone;
+
+	// Use this for initialization
+	void Start () {
+		CurrTime = MaxTime;
+	}
+
+	void SetTime(){
+		TimeSlider.value = CurrTime;	
+		FillImage.color = Color.Lerp (LowTimeColour, FullTimeColour, CurrTime / MaxTime);
+	}
+
+	public void ResetTimer(){
+		CurrTime = MaxTime;
+		SetTime ();
+	}
+
+	public void DecTime(float value){
+
+		CurrTime -= 0.2f;
+		SetTime ();
+		if (CurrTime <= 0) {
+			EndLevel ();
+			ResetTimer();
+			//change player
+		}
+
+	}
+
+	void EndLevel(){
+
+	}
+
+	void Update () {
+		DecTime (0.2f);
+	}
+}
